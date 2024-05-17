91c9538102bdc6c8815d74f0a94078374de10244
Matthews Britto <luismatthews29@gmail.com>
2024-05-17 15:37:31 -0300

    first commit

---
+++ constants/data.js
@@ -0,0 +1,213 @@
+import { Paintbrush, Layers, FileJson } from "lucide-react";
+
+export const LINKS = [
+  {
+    title: "home",
+    path: "/",
+  },
+  {
+    title: "portfolio",
+    path: "/portfolio",
+  },
+  {
+    title: "contact",
+    path: "/contact",
+  },
+];
+
+export const ACCORDION = [
+  {
+    question: "Who are you and what do you do?",
+    answer:
+      "I'm Steve Parker, a passionate developer with a keen interest in development.",
+  },
+  {
+    question: "What motivates you in your career?",
+    answer: "My passion, driving me to continually grow and excel in my career.",
+  },
+  {
+    question: "Refund policy on cancellations?",
+    answer: "Refunds available per our policy.",
+  },
+  {
+    question: "How do you approach problem-solving?",
+    answer: "I tackle problems methodically, breaking them down into smaller tasks.",
+  },
+  {
+    question: "What technologies are you proficient in?",
+    answer: "I'm proficient in JavaScript, HTML, CSS, and various frameworks.",
+  },
+  {
+    question: "Can you describe your communication style?",
+    answer: "I communicate clearly and openly, ensuring everyone is on the same page.",
+  }
+];
+
+
+export const EDUCATION = [
+  {
+    title: "Full Stack Development - ABC University, USA",
+    year: "2011",
+  },
+  {
+    title: "FontEnd Development - ABC University, Ldondon",
+    year: "2009",
+  },
+  {
+    title: "Graphic Designing - ABC University, Turkey",
+    year: "2006",
+  },
+];
+export const EXPERINCE = [
+  {
+    title: "UX/UI Designer - XYZ Company",
+    year: "2012 - 2023",
+  },
+  {
+    title: "Web Developer - XYZ Company",
+    year: "2010 - 2012",
+  },
+];
+
+// SERVICES SECTION
+export const SERVICES = [
+  {
+    icon: <Paintbrush size={44} strokeWidth={0.8} />,
+    title: "FrontEnd Development",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis, aspernatur fugit! Dicta quo quidem soluta.",
+  },
+  {
+    icon: <FileJson size={44} strokeWidth={0.8} />,
+    title: "BackEnd Development",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis, aspernatur fugit! Dicta quo quidem soluta.",
+  },
+  {
+    icon: <Layers size={44} strokeWidth={0.8} />,
+    title: "FullStack Development",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis, aspernatur fugit! Dicta quo quidem soluta.",
+  },
+];
+
+// Work data
+export const WORKDATA = [
+  {
+    title: "frontEnd Website",
+    url: "/img1.png",
+    category: "frontEnd",
+    des: "Interactive, responsive, scalable full-stack website with intuitive user interface design.",
+  },
+  {
+    title: "frontEnd Website",
+    url: "/img2.png",
+    category: "frontEnd",
+    des: "Interactive, responsive, scalable full-stack website with intuitive user interface design.",
+  },
+  {
+    title: "frontEnd Application",
+    url: "/img3.png",
+    category: "frontEnd",
+    des: "Interactive, responsive, scalable full-stack website with intuitive user interface design.",
+  },
+  {
+    title: "frontEnd Application",
+    url: "/img4.png",
+    category: "frontEnd",
+    des: "Interactive, responsive, scalable full-stack website with intuitive user interface design.",
+  },
+  {
+    title: "Website",
+    url: "/img4.png",
+    category: "backEnd",
+    des: "Interactive, responsive, scalable full-stack website with intuitive user interface design.",
+  },
+  {
+    title: "Website",
+    url: "/img3.png",
+    category: "backEnd",
+    des: "Interactive, responsive, scalable full-stack website with intuitive user interface design.",
+  },
+  {
+    title: "Application",
+    url: "/img1.png",
+    category: "backEnd",
+    des: "Interactive, responsive, scalable full-stack website with intuitive user interface design.",
+  },
+  {
+    title: "Application",
+    url: "/img5.png",
+    category: "backEnd",
+    des: "Interactive, responsive, scalable full-stack website with intuitive user interface design.",
+  },
+  {
+    title: "fullStack Website",
+    url: "/img5.png",
+    category: "fullStack",
+    des: "Interactive, responsive, scalable full-stack website with intuitive user interface design.",
+  },
+  {
+    title: "fullStack Website",
+    url: "/img1.png",
+    category: "fullStack",
+    des: "Interactive, responsive, scalable full-stack website with intuitive user interface design.",
+  },
+  {
+    title: "fullStack Application",
+    url: "/img2.png",
+    category: "fullStack",
+    des: "Interactive, responsive, scalable full-stack website with intuitive user interface design.",
+  },
+  {
+    title: "fullStack Application",
+    url: "/img3.png",
+    category: "fullStack",
+    des: "Interactive, responsive, scalable full-stack website with intuitive user interface design.",
+  },
+];
+
+// TESTIMONIAL SECTION
+export const TESTIMONIAL = [
+  {
+    title: "James Martinez",
+    profession: "Director, Nova Gold",
+    url: "/person-1.jpg",
+    des: "Fantastic destinations, knowledgeable guides, seamless logistics. Spectacular views and memories. Exceeded expectations; worth every penny",
+  },
+  {
+    title: "Sophia Anderson",
+    profession: "Manager, Safe City",
+    url: "/person-2.jpg",
+    des: "Unforgettable experiences, friendly staff, and top-notch organization. Highly recommended for adventurous souls. A must-try. Highly recommend",
+  },
+  {
+    title: "Lily Walker",
+    profession: "Director, High Gold",
+    url: "/person-3.jpg",
+    des: "Unbelievable sights, professional guides, seamless planning. Unmatched experiences that leave you wanting more. Highly recommended",
+  },
+  {
+    title: "Ninna Williams",
+    profession: "CEO, Rafa Builds",
+    url: "/person-4.jpg",
+    des: "Unparalleled expertise, personalized service, breathtaking destinations. Consistently delivers an extraordinary travel experience.",
+  },
+  {
+    title: "Ali Khan",
+    profession: "CEO, High Chase",
+    url: "/person-5.jpg",
+    des: "Outstanding journeys, knowledgeable guides, seamless execution. Each trip exceeds expectations, ensuring lasting memories and remarkable experiences.",
+  },
+];
+
+export const SOCIALS = {
+  title: "Social",
+  links: [
+    "/facebook.svg",
+    "/instagram.svg",
+    "/twitter.svg",
+    "/youtube.svg",
+    "/linkedin.svg",
+  ],
+};
